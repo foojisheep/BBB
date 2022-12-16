0f2d37cca80ff6cf908a56f7908e5ed74a3a30f9
Kyli Foo <kylifoo@Kylis-MacBook-Pro.local>
2022-12-17 01:15:48 +0800

    landing page

--- src/landingPage.js
+++ src/landingPage.js
@@ -4,52 +4,61 @@ import logoEnd from './resource/logo-end.svg';
 import './landingPage.css';
 import './loadingPage.css';
 import './workPage.css';
+import ProjectContent from "./projectContent";
 import { Navbar, Nav } from 'rsuite';
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function LandingPage() {    
-  const logos = [logoMiddle,logoEnd,logoStart,logoMiddle,logoEnd,logoStart,logoMiddle,logoEnd,logoStart];
-  // const logos = [logoStart,logoStart,logoStart];
+  // const logos = [logoMiddle,logoEnd,logoStart,logoMiddle,logoEnd,logoStart,logoMiddle,logoEnd,logoStart];
+  const logos = [logoStart,logoMiddle,logoEnd];
     const logo = logos.map((logo,index) => (
-      <img key={index} className="loadingPage-image" src={logo} alt={logo.toString()} />
+      <img key={index} className="landingPage-image" src={logo} alt={logo.toString()} />
     ));
     return(
       <div>
-      <nav className="nav">
-      <Link to="/">BBB
-      </Link>
-      {/* <ul>
-        <CustomLink>Work</CustomLink>
-        <CustomLink>About</CustomLink>
-      </ul> */}
-    </nav>
+        <div className='topnav row'>
+          <div className='nav col-2'> 
+            <h1>Studio BBB</h1>
+           </div>
+           <div className='descp col-8'> 
+           <ul>
+            <li className='row descrip'>is an interior design studio believes every space</li>
+            <li className='row descrip'>created is an expression of oneself.</li>
+          </ul>
 
-    {/* <Nav>
-      <Nav.Item className='nav'>Home</Nav.Item>
-      <Nav.Item>News</Nav.Item>
-      <Nav.Item>Products</Nav.Item>
-    </Nav>
-    <Nav pullRight>
-      <Nav.Item>Settings</Nav.Item>
-    </Nav> */}
-  
+          {/* Previous way of displaying desciption - but have issue with putting center for entire description }
+              {/* <p className='row descrip top-desc'>is an interior design studio believes every space</p>
+              <p className='row descrip bottom-desc'>created is an expression of oneself.</p> */}
 
-        {/* <div className="loadingPage-full">
-          {logo}
+          </div>
+          <div className='topnav-right col-1'>
+            {/* <ul>
+              <li><a href="#">Work</a></li>
+              <li><a href="#">Information</a></li>
+            </ul> */}
+              <a>Work</a>
+            </div>
+            <div className='topnav-right col-1'>
+              <a>Information</a>
+            </div>
+        </div>
+        {/* <div className="projectDisplay">
+          <table id="nav">
+            <tr>
+              <td><h1>Studio BBB</h1></td>
+              <td><ul>
+            <li className='row descrip'>is an interior design studio believes every space</li>
+            <li className='row descrip'>created is an expression of oneself.</li>
+          </ul></td>
+              <td>Not your average neighourhood cafe.</td>
+              <td>Hospo</td>
+            </tr>
+          </table>
         </div> */}
+        <div className="landingPage-full">
+          {logo}
+        </div>
+        <ProjectContent />
       </div>
       );
-
-      function CustomLink({ to, children, ...props }) {
-        const resolvedPath = useResolvedPath(to)
-        const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-      
-        return (
-          <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
-              {children}
-            </Link>
-          </li>
-        )
-      }
 }
---
+++ src/projectContent.js
@@ -0,0 +1,72 @@
+import logoStart from './resource/logo-start.svg';
+import logoMiddle from './resource/logo-middle.svg';
+import logoEnd from './resource/logo-end.svg';
+import './landingPage.css';
+import './loadingPage.css';
+import './workPage.css';
+
+export default function LandingPage() {
+    const content = projects.map((projects) =>
+    <tr key={projects.id}>
+        <td>  </td>
+        <td>{projects.year}</td>
+        <td>{projects.name}</td>
+        <td>{projects.details}</td>
+        <td>{projects.category}</td>
+    </tr>
+    // <div key={post.id}>
+    //     <h3>{post.title}</h3>
+    //     <p>{post.content}</p>
+    // </div>
+    );
+      return(
+          <div className="projectDisplay">
+            <table id="customers">
+                {content}
+              {/* <tr key='0'>
+                <td>  </td>
+                <td>2022</td>
+                <td>Bolt & Witchwood</td>
+                <td>Not your average neighourhood cafe.</td>
+                <td>Hospo</td>
+              </tr>
+              <tr key='1'>
+                <td>  </td>
+                <td>    </td>
+                <td>Poppykat</td>
+                <td>Recalled a certain aesthetic from Wes Anderson's Movie.</td>
+                <td>Hospo</td>
+              </tr>
+              <tr key='2'>
+                <td>  </td>
+                <td>    </td>
+                <td>Kedai Coklat</td>
+                <td>Invokes a precious memory of broken chocholate bars.</td>
+                <td>Hospo</td>
+              </tr>
+              <tr key='3'>
+                <td>  </td>
+                <td>2021</td>
+                <td>Hejau MK</td>
+                <td>Ecovative of sunny Palm Springs and Arizon Cliffs combines.</td>
+                <td>Resi</td>
+              </tr>
+              <tr key='4'>
+                <td>  </td>
+                <td>2020</td>
+                <td>Hejau PJ</td>
+                <td>Reminiscent of a small oasis.</td>
+                <td>Hospo</td>
+              </tr> */}
+            </table>
+          </div>
+        );
+  }
+
+  const projects = [
+    {id: 1, year: '  2022', name: 'Bolt & Witchwood', details: 'Not your average neighourhood cafe.', category: 'Hospo'},
+    {id: 2, year: '      ', name: 'Poppykat', details: 'Recalled a certain aesthetic from Wes Anderson\'s Movie.', category: 'Hospo'},
+    {id: 3, year: '      ', name: 'Kedai Coklat', details: 'Invokes a precious memory of broken chocholate bars.', category: 'Hospo'},
+    {id: 4, year: '  2021', name: 'Hejau MK', details: 'Ecovative of sunny Palm Springs and Arizon Cliffs combines.', category: 'Resi'},
+    {id: 5, year: '  2020', name: 'Hejau PJ', details: 'Reminiscent of a small oasis.', category: 'Hospo'}
+  ];
