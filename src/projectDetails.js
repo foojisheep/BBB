820e3a2ca43e8612d87d28ac644a9ed158d2908f
Kyli Foo <kyli.kyli.foo@gmail.com>
2023-04-30 23:15:09 +0800

    Added arrow right

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -63,7 +63,10 @@ function ShowImage1(props) {
         {/* <LazyLoadImage src={arrow} /> */}
         {/* </HorizontalScroll> */}
       </div>
+      <div className='iconArrow' style={{ position: 'absolute', right: '5%', top: '50%'}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" x2="16" y1="12" y2="12"></line></svg>
       </div>
+    </div>
     </>
   );
 }
