2b9c9754e3d3a3e13be0059bcb853f4371d226a4
Kyli Foo <kyli.kyli.foo@gmail.com>
2023-05-01 23:27:55 +0800

    Modified arrow background colour

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -63,7 +63,7 @@ function ShowImage1(props) {
         {/* <LazyLoadImage src={arrow} /> */}
         {/* </HorizontalScroll> */}
         <div className='iconArrow' style={{ position: 'absolute', right: '5%', bottom: '5%'}}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" x2="16" y1="12" y2="12"></line></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-circle"><circle cx="12" cy="12" r="10" style={{ fill: 'white'}}></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" x2="16" y1="12" y2="12"></line></svg>
         </div>
       </div>
     </div>
@@ -297,7 +297,7 @@ function ShowImage(props) {
          {showImage}
         </HorizontalScroll>
         <div className='iconArrow' style={{ position: 'absolute', right: '5%', bottom: '5%'}}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" x2="16" y1="12" y2="12"></line></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-circle"><circle cx="12" cy="12" r="10" style={{ fill: 'white'}}></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" x2="16" y1="12" y2="12"></line></svg>
         </div>
       </div>
       </div>
